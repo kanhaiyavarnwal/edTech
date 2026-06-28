fbb8b756288348b396bf49301d16bb0a0bccdc71
kanhaiyavarnwal <varnwalkanhaiya@gmail.com>
2026-06-28 10:05:12 +0530

    contact routes is now working

--- src/routes/contactRoutes.js
+++ src/routes/contactRoutes.js
@@ -1,11 +1,10 @@
 
-
 import express from "express"
-import { contactUs } from "../controllers/Contact.js"
+
+import {contactUs} from "../controllers/Contact.js"
 
 const router = express.Router()
 
-router.post("/contact", contactUs)
-
+router.post("/contact",contactUs)
 
 export default router
